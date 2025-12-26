90f14a925996487c83e94fa3a7745b37002ddb87
FoxSeventeen <1661863496@qq.com>
2025-12-27 03:23:54 +0800

    添加配方

--- src/main/java/com/example/fabricdocs/registry/ExampleModItems.java
+++ src/main/java/com/example/fabricdocs/registry/ExampleModItems.java
@@ -1,11 +1,17 @@
 package com.example.fabricdocs.registry;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
 
 public class ExampleModItems implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        CompostingChanceRegistry.INSTANCE.add(ModItems.SUSPICIOUS_SUBSTANCE, 1.0f);
+        FuelRegistryEvents.BUILD.register((builder, context) -> {
+            builder.add(ModItems.SUSPICIOUS_SUBSTANCE, 30 * 20);
+        });
         ModItems.initialize();
     }
 }
